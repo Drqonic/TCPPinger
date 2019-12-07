a888d5398189df6ebad1b3fcfb0662d65afab6f3
Misfortune <36631640+MisfortuneIsLit@users.noreply.github.com>
2019-12-07 00:17:03 +0000

    Update tcp_ping.py

--- tcp_ping.py
+++ tcp_ping.py
@@ -3,7 +3,7 @@ import time
 import socket
 
 if len(sys.argv) < 3:
-	sys.exit("Usage: python {} [Host] [Port]".format(sys.argv[0]))
+	sys.exit("Usage: python {} <Host> <Port>".format(sys.argv[0]))
 
 def get_ip(host):
 	try:
@@ -18,14 +18,22 @@ while True:
 	try:
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.settimeout(3)
+
 		start = time.time() * 1000
-		sock.connect((host, port))
+		
+		resp = sock.connect_ex((host, port))
+		
 		stop = int(time.time() * 1000 - start)
-		print("Probing {}:{}/TCP - Port is open | Time={}ms".format(host, port, stop))
+
+		if resp == 0:
+			print("Probing {}:{}/TCP - Port is open | Time={}ms".format(host, port, stop))
+			time.sleep(1)
+		else:
+			print("Probing {}:{}/TCP - Port is closed".format(host, port))
+			time.sleep(3)
+
 		sock.close()
-		time.sleep(1)
 	except socket.error:
-		print("Probing {}:{}/TCP - Port is closed".format(host, port))
-		time.sleep(1)
+		print("Socket failure...")
 	except KeyboardInterrupt:
 		break
