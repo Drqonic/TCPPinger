14beb9abbfe8d1a01aa70f16d78490f34c4b7ba6
Misfortune <36631640+Drqonic@users.noreply.github.com>
2021-10-19 18:06:38 +0100

    Update tcp_ping.py
    Changed to follow PEP8 character limit.

--- tcp_ping.py
+++ tcp_ping.py
@@ -1,5 +1,4 @@
 import argparse
-import random
 import socket
 import sys
 import threading
@@ -24,13 +23,12 @@ def get_ip(host, family=socket.AF_INET):
 	"""
 
 	try:
-		if family == socket.AF_INET:
-			return socket.getaddrinfo(host, None, socket.AF_INET)[0][4][0]
-
 		if family == socket.AF_INET6:
 			return socket.getaddrinfo(host, None, socket.AF_INET6)[0][4][0]
-
-		return socket.gethostbyname(host)
+		elif family == socket.AF_INET:
+			return socket.getaddrinfo(host, None, socket.AF_INET)[0][4][0]
+		else:
+			return socket.gethostbyname(host)
 	except socket.gaierror:
 		sys.exit("The host's IP address was not retrievable.")
 
@@ -63,7 +61,11 @@ class TCPPinger(threading.Thread):
 		
 	"""
 
-	def __init__(self, host, port, quantity=4, timeout=3000, sleep=1000, family=socket.AF_INET):
+	def __init__(
+		self, host, port,
+		quantity=4, timeout=3000, sleep=1000,
+		family=socket.AF_INET
+	):
 		"""
 		Parameters
 		----------
@@ -92,20 +94,9 @@ class TCPPinger(threading.Thread):
 
 	def run(self):
 		"""
-		Attributes
-		----------
-		amount_looped : int
-			A record of how many times we have probed the host.
-		addr : tuple
-			A combination of the host and port together
-		sock : socket.socket
-			An endpoint we will use to connect to the host.
-		start : float
-			The current time of which we started probing the host.
-		now : float
-			The amount of time took trying to connect to the host.
+		This is the main method that is used for pinging the host.
 		"""
-
+		
 		amount_looped = 0
 
 		while amount_looped < self.quantity or self.quantity == -1:
@@ -123,9 +114,13 @@ class TCPPinger(threading.Thread):
 				if sock.connect_ex(addr) == 0:
 					now = int(time.time() * 1000 - start)
 
-					print("Probing {}:{}/TCP - Port is open | Time={}ms".format(self.host, self.port, now))
+					print("Probing {}:{}/TCP - Port is open | Time={}ms".format(
+						self.host, self.port, now
+					))
 				else:
-					print("Probing {}:{}/TCP - Port is closed".format(self.host, self.port))
+					print("Probing {}:{}/TCP - Port is closed".format(
+						self.host, self.port
+					))
 
 				sock.close()
 			except socket.error:
@@ -137,22 +132,69 @@ class TCPPinger(threading.Thread):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("host", action="store", help="Host to connect to")
-	parser.add_argument("-port", "-p", type=int, required=True, help="Which port to connect with")
-	parser.add_argument("-num", "-n", type=int, default=4, help="Amount of times to probe the host")
-	parser.add_argument("-timeout", "-t", type=int, default=3000, help="How long we should try to connect for until it returns an error (MS)")
-	parser.add_argument("-sleep", "-s", type=int, default=1000, help="Delay until next TCP request (MS)")
-	parser.add_argument("-loop", "-l", default=False, action="store_true", help="Constantly pinging the host (Even if number specified)")
-	parser.add_argument("-ipv4", "-4", default=True, action="store_true", help="Uses IPv4")
-	parser.add_argument("-ipv6", "-6", default=False, action="store_true", help="Uses IPv6")
+	parser.add_argument(
+		"host",
+		action="store",
+		help="Host to connect to"
+	)
+	parser.add_argument(
+		"-port",
+		"-p",
+		type=int,
+		required=True,
+		help="Which port to connect with"
+	)
+	parser.add_argument(
+		"-num",
+		"-n",
+		type=int,
+		default=4,
+		help="Amount of times to probe the host"
+	)
+	parser.add_argument(
+		"-timeout",
+		"-t",
+		type=int,
+		default=3000,
+		help="How long we should try to connect for until it returns an error (MS)"
+	)
+	parser.add_argument(
+		"-sleep",
+		"-s",
+		type=int,
+		default=1000,
+		help="Delay until next TCP request (MS)"
+	)
+	parser.add_argument(
+		"-loop",
+		"-l",
+		default=False,
+		action="store_true",
+		help="Constantly pinging the host (Even if number specified)"
+	)
+	parser.add_argument(
+		"-ipv4",
+		"-4",
+		default=True,
+		action="store_true",
+		help="Uses IPv4"
+	)
+	parser.add_argument(
+		"-ipv6",
+		"-6",
+		default=False,
+		action="store_true",
+		help="Uses IPv6"
+	)
 
 	args = parser.parse_args()
 
-	if args.ipv4 is True:
-		family = socket.AF_INET
-	
 	if args.ipv6 is True:
 		family = socket.AF_INET6
+	elif args.ipv4 is True:
+		family = socket.AF_INET
+	else:
+		sys.exit("An unknown error has occurred.")
 
 	if args.loop is True:
 		args.num = -1
@@ -160,9 +202,15 @@ if __name__ == "__main__":
 	host = get_ip(args.host, family)
 
 	if args.host != host:
-		print("Starting to probe {} ({}) on port {}".format(args.host, host, args.port))
+		print("Starting to probe {} ({}) on port {}".format(
+			args.host, host, args.port
+		))
 	else:
 		print("Starting to probe {} on port {}".format(args.host, args.port))
 
-	pinger = TCPPinger(host, args.port, args.num, args.timeout, args.sleep, family)
+	pinger = TCPPinger(
+		host, args.port,
+		args.num, args.timeout, args.sleep,
+		family
+	)
 	pinger.start()
